9ae4ac78f55d3c4d4947a2a76eeabb65edc18b74
likhithan2001 <138875891+9ilu@users.noreply.github.com>
2024-06-11 21:47:06 +0530

    commit

---
+++ mon.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+#
+#Author:Likhitha
+#Date:03/6/2024
+#
+#This script outputs the node health
+#
+set -x 
+########
+set -e
+########
+set -o pipefail
+#################
+echo "Print the disk space"
+df -h
+
+##############
+echo "Print the memory"
+free -g
+
+############
+
